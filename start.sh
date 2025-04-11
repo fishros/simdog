cacef2037e1dc580073ec1f5ad7406936986ebca
MoXins <2401038163@qq.com>
2025-04-11 19:28:29 +0800

    add gps

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 WAIT_TIME=8
 
 # 启动Gazebo仿真环境 3d_to_2d slamtoolbox
-gnome-terminal -- bash -c "source install/setup.bash && ros2 launch go2_config gazebo_velodyne.launch.py rviz:=true; exec bash"
+gnome-terminal -- bash -c "source install/setup.bash && ros2 launch go2_config gazebo_velodyne.launch.py rviz:=false; exec bash"
 # gnome-terminal -- bash -c "source install/setup.bash && ros2 launch pointcloud_to_laserscan pointcloud_to_laserscan_launch.py; exec bash"
 # gnome-terminal -- bash -c "source install/setup.bash && ros2 launch go2_config slam.launch.py; exec bash"   #启动slam_toolbox建图模式，如果需要定位，可以在配置文件里改，然后将导航的amcl注释掉
 # gnome-terminal -- bash -c "source install/setup.bash && ros2 launch go2_config navigate.launch.py; exec bash"  #启动导航 暂时未配好 等待github 更新
